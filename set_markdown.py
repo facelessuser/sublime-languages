e34a1c256f3e6c0b91f1cac3907ddfab5a1d3019
facelessuser <faceless.shop@gmail.com>
2014-06-23 19:30:43 -0600

    Allow switching of default markdown type

---
+++ set_markdown.py
@@ -0,0 +1,33 @@
+import sublime
+import sublime_plugin
+
+extensions = [
+    "mdown",
+    "markdown",
+    "markdn",
+    "md"
+]
+
+
+class MardownSwitcherCommand(sublime_plugin.WindowCommand):
+    markdowns = [
+        "Markdown",
+        "MultiMarkdown"
+        # "GithubFlavoredMarkdown"
+    ]
+
+    def run(self):
+        self.window.show_quick_panel(
+            self.markdowns,
+            self.set_language
+        )
+
+    def set_language(self, value):
+        if value > -1:
+            for x in range(len(self.markdowns)):
+                settings = sublime.load_settings("%s.sublime-settings" % self.markdowns[x])
+                if value == x:
+                    settings.set("extensions", extensions)
+                else:
+                    settings.set("extensions", [])
+                sublime.save_settings("%s.sublime-settings" % self.markdowns[x])
